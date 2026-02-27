224dc197e1768f5ce32ee4da42589aa507977698
Rishikesh099 <rishikeshbnair@gmail.com>
2026-02-28 00:06:22 +0530

    lod rou

--- app.py
+++ app.py
@@ -18,11 +18,6 @@ def home():
 def login():
     return render_template('login.html')
 
-# @app.route('/login')
-# def login_post():
-#     return
-     
-
 @app.route('/company-signup')
 def company_signup():
     return render_template('companysignup.html')
@@ -31,8 +26,25 @@ def company_signup():
 def user_signup():
     return render_template('usersignup.html')
 
+@app.route('/login', methods=['POST'])
+def login_post():
+    email = request.form['email']
+    password = request.form['password']
+    role = request.form['role']
 
 
+    conn = sqlite3.connect(os.path.join(current_dir, 'database.db'))
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM USERS WHERE email=? AND pass=? AND usertype=?", (email, password, role))
+    user = cursor.fetchone()
+    conn.close()
 
+    if user:
+        if role == 0 and user[1] == email and user[2] == password:  # Company user
+            return render_template('companydashboard.html', user=user)
+        elif role == 1 and user[1] == email and user[2] == password:  # Regular user 
+            return render_template('userdashboard.html', user=user)
+        
+        
 if __name__ == '__main__':
     app.run(debug=True)
